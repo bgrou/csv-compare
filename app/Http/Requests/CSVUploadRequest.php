1599b76527d30473a12f5b77bdfd5e864c837711
Bruno Grou <bruno.grou@hotmail.com>
2023-10-22 18:23:54 +0100

    Created Form Request with validation

---
+++ app/Http/Requests/CSVUploadRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CSVUploadRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'old_csv' => 'required|mimes:csv,txt|max:150000',
+            'new_csv' => 'required|mimes:csv,txt|max:150000'
+        ];
+    }
+}
